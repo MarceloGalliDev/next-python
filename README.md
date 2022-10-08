f38acc9d9bd005244c6f8d06a055ea7106ba7c63
marcelo <marcelolemesgalli@hotmail.com>
2022-10-08 12:34:14 -0300

    update
    update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # next-python
  Project in react-next-typescript-python
+
+> Habilitar a execução de scripts no Windows
+    - Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+    
+> Criar o projeto
+    - npx create-next-app . --ts
+
+> Framework
+    - Material UI - npm i @mui/material@5.10.2 @emotion/react@11.10.0 @emotion/styled@11.10.0
---
+++ frontend/pages/index.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from 'next'
+
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      Galli brothers
+    </div>
+  )
+}
+
+export default Home
+
+
+//criando componente
+//so realizar a tipagem do props
+/*
+function Button(props) {
+  return <a href="#">{props.title}</a>
+}
+
+<Button title="click" /><br />
+<Button title="Push" /><br />
+<Button title="Pull" /><br />
+
+*/
